e970cb00323484dca1ff0ceed9d8aecfc7c8001b
Stefan Bayne <stefanbayne@gmail.com>
2022-11-16 12:50:33 -0500

    First commit.

---
+++ HackerNews/HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Stefan Bayne on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
